5af33a07f9b2c010a7edf72d08aa35b6428c03ba
Suui <suui.gd@gmail.com>
2016-10-16 21:04:44 +0200

    Fresh simple start

--- Tutorial.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "TutorialConfig.h"
-
-int main(int argc, char* argv[])
-{
-	std::cout << "Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
-	std::cout << "Hello world!" << std::endl;
-	return 0;
-}
