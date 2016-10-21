d527e066a5d343e8c7cacf083102a6cd6c413c1f
Suui <suui.gd@gmail.com>
2016-10-21 23:24:33 +0200

    Refactor introduce SetUp in test

--- SlothUnit/SlothUnit.Parser.Test/ClangWrapperShould.cs
+++ SlothUnit/SlothUnit.Parser.Test/ClangWrapperShould.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Linq;
+using ClangSharp;
 using FluentAssertions;
 using NUnit.Framework;
 using SlothUnitParser;
@@ -14,34 +15,32 @@ namespace SlothUnit.Parser.Test
 	[TestFixture]
 	class ClangWrapperShould : FileSystemTest
 	{
-		[Test]
-		public void retrieve_the_name_of_a_cursor()
+		private CXCursor ClassCursor { get; set; }
+		private string FilePath { get; set; }
+
+		[SetUp]
+		public void given_a_class_cursor_in_a_file()
 		{
-			var filePath = Path.Combine(TestProjectDir, "ClangWrapperShould.h");
-			var clangWrapper = new ClangWrapper();
-			var classCursor = clangWrapper.GetClassCursorsIn(filePath).Single();
-
-			ClangWrapper.GetCursorName(classCursor).Should().Be("ClangWrapperShould");
-		}
-
-		[Test]
-		public void retrieve_the_line_of_a_cursor()
-		{
-			var filePath = Path.Combine(TestProjectDir, "ClangWrapperShould.h");
-			var clangWrapper = new ClangWrapper();
-			var classCursor = clangWrapper.GetClassCursorsIn(filePath).Single();
-
-			ClangWrapper.GetCursorLine(classCursor).Should().Be(6);
+			FilePath = Path.Combine(TestProjectDir, "ClangWrapperShould.h");
+			ClassCursor = new ClangWrapper().GetClassCursorsIn(FilePath).Single();
 		}
 
 		[Test]
 		public void retrieve_the_filepath_for_the_file_a_cursor_is_in()
 		{
-			var filePath = Path.Combine(TestProjectDir, "ClangWrapperShould.h");
-			var clangWrapper = new ClangWrapper();
-			var classCursor = clangWrapper.GetClassCursorsIn(filePath).Single();
+			ClangWrapper.GetCursorFilePath(ClassCursor).Should().Be(FilePath);
+		}
 
-			ClangWrapper.GetCursorFilePath(classCursor).Should().Be(filePath);
+		[Test]
+		public void retrieve_the_name_of_a_cursor()
+		{
+			ClangWrapper.GetCursorName(ClassCursor).Should().Be("ClangWrapperShould");
+		}
+
+		[Test]
+		public void retrieve_the_line_of_a_cursor()
+		{
+			ClangWrapper.GetCursorLine(ClassCursor).Should().Be(6);
 		}
 	}
 }
