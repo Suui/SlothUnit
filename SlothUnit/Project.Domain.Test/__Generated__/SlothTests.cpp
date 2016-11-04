8053cddf7ae9bf9718e29d8b68cf92200ae0dbb9
Suui <suui.gd@gmail.com>
2016-11-04 01:37:16 +0100

    Global object registration

--- SlothUnit/Project.Domain.Test/__Generated__/SlothTests.cpp
+++ SlothUnit/Project.Domain.Test/__Generated__/SlothTests.cpp
@@ -2,27 +2,16 @@
 #include "TestClass.h"
 #include "../CodeGenerationShould/CalculatorShould.h"
 #include <unordered_map>
-#include <functional>
 
-void voidFunction() {}
+TestClass<CalculatorShould> calculatorShould = TestClass<CalculatorShould>("SuperPath/CalculatorShould.h",
+											   "CalculatorShould",
+											   CalculatorShould(),
+											   {
+											       { "return_four_when_adding_two_plus_two", &CalculatorShould::return_four_when_adding_two_plus_two },
+											       { "a_method_with_an_annotation", &CalculatorShould::a_method_with_an_annotation }
+											   });
 
 void SlothTests::Run()
 {
-	typedef std::function<void()> TestFunction;
-	auto testClassX = TestClass<CalculatorShould>("SuperPath/CalculatorShould.h",
-												  "CalculatorShould",
-												  CalculatorShould(),
-												  {
-													  { "return_four_when_adding_two_plus_two", &CalculatorShould::return_four_when_adding_two_plus_two },
-													  { "a_method_with_an_annotation", &CalculatorShould::a_method_with_an_annotation }
-												  });
-
-//	std::unordered_map<std::string, testFunction> testMethods;
-//	CalculatorShould calc;
-//	testMethods.emplace("setup", std::bind(&voidFunction));
-//	testMethods.emplace("return_four_when_adding_two_plus_two", std::bind(&CalculatorShould::return_four_when_adding_two_plus_two, calc));
-//	testMethods.emplace("a_method_with_an_annotation", std::bind(&CalculatorShould::a_method_with_an_annotation, calc));
-//
-//	std::unordered_map<std::string, std::shared_ptr<CalculatorShould>> testClasses;
-//	testClasses.emplace("CalculatorShould", std::make_shared<CalculatorShould>());
+	
 }
--- SlothUnit/Project.Domain.Test/__Generated__/SlothTests.h
+++ SlothUnit/Project.Domain.Test/__Generated__/SlothTests.h
@@ -1,4 +1,16 @@
 ﻿#pragma once
+#include <iostream>
+
+class Initialize
+{
+public:
+
+	Initialize()
+	{
+		std::cout << "Initialized!" << std::endl;
+	}
+
+};
 
 class SlothTests
 {
